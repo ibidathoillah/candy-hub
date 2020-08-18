d088e13d744c16f617323879d3b81da340600acb
mac <mac@macs-MacBook-Pro.local>
2020-08-19 01:29:01 +0700

    update

--- resources/build/js/candyhub.js
+++ resources/build/js/candyhub.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1321);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1316);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 1321:
+/***/ 1316:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(64);
