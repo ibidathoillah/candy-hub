dcb8a94aa00173cbf4332822fac0e9e808aedce9
mac <mac@macs-MacBook-Pro.local>
2020-08-19 00:45:35 +0700

    update

--- src/Services/Navigation/Menus/OrderProcessing/SideMenu.php
+++ src/Services/Navigation/Menus/OrderProcessing/SideMenu.php
@@ -14,8 +14,8 @@ class SideMenu extends Menu
             // new NavItem('Returns', route('hub.orders.index')),
             // new NavItem('hub::titles.subsribers', route('hub.subsribers.index'), 'subsribers'),
             new NavItem('hub::titles.customers', route('hub.customers.index'), 'customers'),
-            new NavItem('hub::titles.shipping_methods', route('hub.shipping.index'), 'shipping_methods'),
-            new NavItem('hub::titles.shipping_zones', route('hub.shipping_zones.index'), 'shipping_zones'),
+            // new NavItem('hub::titles.shipping_methods', route('hub.shipping.index'), 'shipping_methods'),
+            // new NavItem('hub::titles.shipping_zones', route('hub.shipping_zones.index'), 'shipping_zones'),
             new NavItem('hub::titles.articles', route('hub.articles.index'), 'articles'),
             new NavItem('hub::titles.settings', route('hub.settings.index'), 'settings'),
         ];
