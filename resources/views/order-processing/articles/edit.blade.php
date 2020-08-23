c6c3861649c0a38da813c8240237677d9fdafd26
mac <mac@macs-MacBook-Pro.local>
2020-08-24 06:07:44 +0700

    update

--- resources/views/order-processing/articles/edit.blade.php
+++ resources/views/order-processing/articles/edit.blade.php
@@ -10,7 +10,6 @@
 @endsection
 
 @section('header_actions')
-    <candy-button class="btn btn-warning" style="display: inline-block;" override="delete">Hapus</candy-button>
     <candy-button style="display: inline-block;" override="save">Simpan</candy-button>
     @verbatim<candy-button style="display: inline-block;" v-if="status== 'Unpublish'" override="Unpublish">{{status}}</candy-button><candy-button style="display: inline-block;" v-if="status== 'Publish'" override="Publish">{{status}}</candy-button>@endverbatim
 @stop
