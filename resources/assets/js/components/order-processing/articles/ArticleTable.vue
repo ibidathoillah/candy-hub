67a668c3d155a6c80c407d28f52f1e08654cea51
mac <mac@macs-MacBook-Pro.local>
2020-08-30 16:59:24 +0700

    update

--- resources/assets/js/components/order-processing/articles/ArticleTable.vue
+++ resources/assets/js/components/order-processing/articles/ArticleTable.vue
@@ -62,13 +62,13 @@
                                 {{ row.title }}
                             </td>
                             <td @click="goTo(row.id)">
-                                {{ row.category.name ?  row.category.name  : "" }}
+                                {{ row.category ?  row.category.name  : "" }}
                             </td>
                             <td @click="goTo(row.id)">
                                 <a href="#">{{ row.slug }}</a>
                             </td>
                             <td @click="goTo(row.id)">
-                                <a href="#">{{ row.type.name  ?  row.type.name  : ""  }}</a>
+                                <a href="#">{{ row.type  ?  row.type.name  : ""  }}</a>
                             </td>
                             <td @click="goTo(row.id)">
                                 <a href="#">{{ row.tags.toString() }}</a>
